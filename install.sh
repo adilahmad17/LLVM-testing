3cd828dbceb3b51d4f4807c3b10823590614aff4
adilahmad17 <adilahmad17@gmail.com>
2019-05-20 18:22:35 +0000

    wip

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash -e
 
+rm -rf build
+mkdir build
+
 pushd build
-cmake -DLLVM_TARGETS_TO_BUILD=X86 -DCMAKE_BUILD_TYPE=MinSizeRel ../llvm-7.0.0.src
-make -j`nproc`
+#cmake -DLLVM_TARGETS_TO_BUILD=X86 -DCMAKE_BUILD_TYPE=MinSizeRel ../llvm-7.0.0.src
+  cmake -DLLVM_TARGETS_TO_BUILD=X86 -DCMAKE_BUILD_TYPE=Release -DLLVM_INCLUDE_TESTS=OFF ../llvm-7.0.0.src
+  make -j`nproc`
 popd
